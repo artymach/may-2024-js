65f138766bc5f9007cd186b337a8fc43a8cafaf5
artymach <artemmachikhin1983@gmail.com>
2024-08-06 12:58:00 +0300

    innit commit

---
+++ Lessons/lesson_1_var_operations/homework_1/main.js
@@ -0,0 +1,56 @@
+//------------task1--------------
+
+let word1 = 'hello';
+console.log(word1);
+let word2 = 'owu';
+console.log(word2);
+let word3 = 'com';
+console.log(word3);
+let word4 = 'ua';
+console.log(word4);
+let num1 = 1;
+console.log(num1);
+let num2 = 10;
+console.log(num2);
+let num3 = -999;
+console.log(num3);
+let num4 = 123;
+console.log(num4);
+const PI = 3.14;
+console.log(PI);
+let num5 = 2.7;
+console.log(num5);
+let yes = true;
+console.log(yes);
+let no = false;
+console.log(no)
+
+// ------------task2-------------
+
+let firstName = 'Artem';
+let middleName = 'Valerievich';
+let lastName = 'Machikhin';
+let person = `${lastName} ${firstName} ${middleName}`;
+console.log(person);
+
+// ------------task3--------------
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+// ------------task4---------------
+
+let firstname = prompt('Enter your name:');
+let lastname = prompt('Enter your lastname:');
+let age = prompt('Enter your age:');
+console.log(firstname);
+console.log(lastname);
+console.log(age)
+
+
+
+
